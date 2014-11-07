4ea161a8c844e8fba30eb62f3c3ad8a7052136d4
akshalaby <akshalaby@gmail.com>
2014-11-06 20:12:14 -0500

    added unity project with infinite terrain

---
+++ Unity_EndlessTerrain/Assets/GroundMove.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+
+public class GroundMove : MonoBehaviour
+{
+	[HideInInspector]
+	public bool facingRight = true;			// For determining which way the player is currently facing.
+	[HideInInspector]
+	
+	
+	public float moveForce = 365f;			// Amount of force added to move the player left and right.
+	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
+
+	private Transform groundCheck;			// A position marking where to check if the player is grounded.
+	private bool grounded = false;			// Whether or not the player is grounded.
+	private Animator anim;					// Reference to the player's animator component.
+	
+	
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	
+	void Update()
+	{
+	}
+	
+	
+	void FixedUpdate ()
+	{
+		// Cache the horizontal input.
+		float h = -Input.GetAxis("Horizontal");
+		
+		// The Speed animator parameter is set to the absolute value of the horizontal input.
+		anim.SetFloat("Speed", Mathf.Abs(h));
+		
+		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+		if(h * rigidbody2D.velocity.x < maxSpeed)
+			// ... add a force to the player.
+			rigidbody2D.AddForce(Vector2.right * h * moveForce);
+		
+		// If the player's horizontal velocity is greater than the maxSpeed...
+		if(Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+			// ... set the player's velocity to the maxSpeed in the x axis.
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+
+	}
+	
+	
+}
+
