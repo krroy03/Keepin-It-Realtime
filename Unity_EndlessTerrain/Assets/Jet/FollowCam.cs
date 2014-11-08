6f8be4ffd0b0fa5f6ece21f21e7255868c81b775
Roy <rikkyroy@gmail.com>
2014-11-08 15:02:28 -0500

    Merged the jet controller into the game, camera follows jet
    Signed-off-by: Roy <rikkyroy@gmail.com>

---
+++ Unity_EndlessTerrain/Assets/Jet/FollowCam.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour
+{
+
+		public GameObject target;
+		// Use this for initialization
+		void Start ()
+		{
+				Camera.main.transform.position = new Vector3 (target.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				Camera.main.transform.position = new Vector3 (target.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+		}
+}
