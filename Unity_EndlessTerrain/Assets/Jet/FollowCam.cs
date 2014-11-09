bb07ab726b9bc4cad640bd3a95e322a9ffc1124e
Roy <rikkyroy@gmail.com>
2014-11-09 15:17:04 -0500

    Added in networking and in game chat, cleaned up some stuff, players can host and join games now
    Signed-off-by: Roy <rikkyroy@gmail.com>

--- Unity_EndlessTerrain/Assets/Jet/FollowCam.cs
+++ Unity_EndlessTerrain/Assets/Jet/FollowCam.cs
@@ -5,6 +5,7 @@ public class FollowCam : MonoBehaviour
 {
 
 		public GameObject target;
+		private GameObject[] jets;
 		// Use this for initialization
 		void Start ()
 		{
@@ -14,6 +15,24 @@ public class FollowCam : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
+				var jets = GameObject.FindGameObjectsWithTag ("Player");
+				// find jet in the front 
+				GameObject inFront = null;
+				for (int i = 0; i < jets.Length; i++) {
+						var jet = jets [i];
+						if (inFront == null) {
+								inFront = jet;
+						} else {
+								if (jet.transform.position.x > inFront.transform.position.x) {
+										inFront = jet;
+								}
+						}
+				}
+
+
+				if (inFront != null) 
+						target = inFront;
+
 				Camera.main.transform.position = new Vector3 (target.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
 		}
 }
